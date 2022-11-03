a357a545d2d94d7ead9152f01e21550bdc65c8d6
dell laptop <dell laptop@Vipul>
2022-11-03 16:22:52 +0530

    for group 24

---
+++ hello/src/helloworld/class1.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class class1 {
+	public static void main(String [] args) {
+		System.out.println("hello world");
+}
+}
---
+++ hello/src/helloworld/class4.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class class4 {
+public static void main(String[] args) {
+	float a=60,b=40,c=80,d=55,addition,multiplication,subtraction,division;
+	addition=a+b+c+d;
+	System.out.println(addition);
+	multiplication=a*b*c*d;
+	System.out.println(multiplication);
+	subtraction=a-b-c-d;
+	System.out.println(subtraction);
+	division=(((a/b)/c)/d);
+	System.out.println(division);
+}
+}
